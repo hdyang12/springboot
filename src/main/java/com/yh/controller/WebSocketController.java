fd687116c8de8b890b0abb7adbdccb9b28f1150a
yanghui <hdyang12@163.com>
2017-02-09 20:25:19 +0800

    webSocket  TODO

---
+++ src/main/java/com/yh/controller/WebSocketController.java
@@ -0,0 +1,18 @@
+package com.yh.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * WebSocket
+ * 1.广播式
+ * 2.点对点
+ * @author yh
+ *
+ */
+@Controller
+public class WebSocketController {
+	
+	//@Messagemapping   需要引入相应jar
+	//TODO
+
+}
