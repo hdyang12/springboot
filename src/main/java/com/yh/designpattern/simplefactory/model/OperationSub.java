61c7eee55777586289ecc6957a9ab4ed17f88428
yanghui <hdyang12@163.com>
2017-11-20 19:35:37 +0800

    设计模式:简单工厂模式

--- src/main/java/com/yh/designpattern/simplefactory/model/OperationSub.java
+++ src/main/java/com/yh/designpattern/simplefactory/model/OperationSub.java
@@ -12,9 +12,6 @@ public class OperationSub extends Operate{
 
 	@Override
 	double operation(double d1, double d2) {
-		if (d2 == 0) {
-			return Double.POSITIVE_INFINITY;
-		}
 		return d1 / d2;
 	}
 
