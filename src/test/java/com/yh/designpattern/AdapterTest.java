4a675b84a90f83e8574ae6b7020fd5509dd1f27e
yanghui <hdyang12@163.com>
2017-11-29 14:55:13 +0800

    适配器模式

--- src/test/java/com/yh/designpattern/AdapterTest.java
+++ src/test/java/com/yh/designpattern/AdapterTest.java
@@ -5,6 +5,13 @@ import org.junit.Test;
 import com.yh.designpattern.adapter.Adapter;
 import com.yh.designpattern.adapter.Target;
 
+/**
+ * 适配器模式
+ * 
+ * 有点亡羊补牢的感觉,最好能事先预防接口不同的问题;在有小问题的时候,及时重构;不至于到了最后无法挽救,只能用适配器模式
+ * @author yh
+ *
+ */
 public class AdapterTest {
 	
 	@Test
